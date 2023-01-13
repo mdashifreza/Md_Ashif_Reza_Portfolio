12c06c910d3fd879cca1762c5fd74a7cfbe0347c
mdashif5@gmail.com <55871288+mdashifreza@users.noreply.github.com>
2023-01-14 03:32:44 +0530

    resume added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../../state/index";
-import cloud from "../../assets/portfolio/cloud.pdf"
+import Md_Ashif_Reza_Resume_Full_Stack_Dev from "../../assets/portfolio/Md_Ashif_Reza_Resume_Full_Stack_Dev.pdf"
 function Navbar() {
     const dispatch = useDispatch();
     const { updateDarkMode } = bindActionCreators(actionCreators, dispatch);
@@ -36,8 +36,8 @@ return (
             <li>
                 <a
                 className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 ml-8 rounded-md mx-1.5 sm:mx-4"
-                href={cloud}
-                download="Md_Ashif_Reza_CV"
+                href={Md_Ashif_Reza_Resume_Full_Stack_Dev}
+                download="Md_Ashif_Reza_Resume_Full_Stack_Dev"
                 >
                 Resume
                 </a>
